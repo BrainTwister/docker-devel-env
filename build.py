3a997dfac1264acac2b3020a9eb8956eb9246308
Bernd Doser <bernd.doser@braintwister.eu>
2018-02-19 16:11:24 +0100

    Image configuration according to the base image
    - Add conan repositories for eclipse user only if the base image contain the conan module.
    - Add eclipse user to docker group only if the base image contain the docker module.

--- build.py
+++ build.py
@@ -1,40 +1,47 @@
 #!/usr/bin/python3
 
+import argparse
 import subprocess
 import yaml
 
-stream = open('images.yml', 'r')
-yaml = yaml.load(stream)
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-i, --images', dest='images', default='images.yml',
+    help='List of docker images to build (default: images.yml)')
+
+args = parser.parse_args()
+yaml = yaml.load(open(args.images, 'r'));
 
 # Build base images
-for base_image in yaml['base-images']:
-
-    image_name = '-'.join(base_image)
-    print(image_name.ljust(90), end='', flush=True)
-
-    cmd = 'docker build -t braintwister/' + image_name + ' .'
-    log = open(image_name + '.log', "w")
-    p = subprocess.run(cmd, shell=True, cwd=image_name, stdout=log, stderr=subprocess.STDOUT)
-
-    if p.returncode == 0:
-        print(' ... passed')
-    else:
-        print(' ... failed')
+if 'base-images' in yaml:
+    for base_image in yaml['base-images']:
+    
+        image_name = '-'.join(base_image)
+        print(image_name.ljust(90), end='', flush=True)
+    
+        cmd = 'docker build -t braintwister/' + image_name + ' .'
+        log = open(image_name + '.log', "w")
+        p = subprocess.run(cmd, shell=True, cwd=image_name, stdout=log, stderr=subprocess.STDOUT)
+    
+        if p.returncode == 0:
+            print(' ... passed')
+        else:
+            print(' ... failed')
 
 # Build images
-for image in yaml['images']:
-
-    image_name = '-'.join(image)
-    print(image_name.ljust(90), end='', flush=True)
-
-    base       = '-'.join(image[:-1])
-    module     = image[-1:][0] 
-
-    cmd = 'docker build -t braintwister/' + image_name + ' --build-arg BASE_IMAGE=braintwister/' + base + ' .'
-    log = open(image_name + '.log', "w")
-    p = subprocess.run(cmd, shell=True, cwd=module, stdout=log, stderr=subprocess.STDOUT)
-
-    if p.returncode == 0:
-        print(' ... passed')
-    else:
-        print(' ... failed')
+if 'images' in yaml:
+    for image in yaml['images']:
+    
+        image_name = '-'.join(image)
+        print(image_name.ljust(90), end='', flush=True)
+    
+        base       = '-'.join(image[:-1])
+        module     = image[-1:][0] 
+    
+        cmd = 'docker build -t braintwister/' + image_name + ' --build-arg BASE_IMAGE=braintwister/' + base + ' .'
+        log = open(image_name + '.log', "w")
+        p = subprocess.run(cmd, shell=True, cwd=module, stdout=log, stderr=subprocess.STDOUT)
+    
+        if p.returncode == 0:
+            print(' ... passed')
+        else:
+            print(' ... failed')
---
+++ conan-1.0/conan_add_repositories.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+conan remote add braintwister https://api.bintray.com/conan/braintwister/conan
+conan remote add conan-community https://api.bintray.com/conan/conan-community/conan
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
