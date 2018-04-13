45284f3bcc33b9586100e33844c89e045ad46ef6
Bernd Doser <bernd.doser@braintwister.eu>
2018-04-13 15:32:41 +0200

    Revert "move install_plugins.py into eclipse-common"
    This reverts commit 0a3c7df43166dd177d2de17f5c54514a4b447e3b.

--- eclipse-cpp-4.8/install_plugins.py
+++ eclipse-cpp-4.8/install_plugins.py
@@ -1,1 +1,25 @@
-../eclipse-common/install_plugins.py
+#!/usr/bin/python3
+
+import subprocess
+import yaml
+
+stream = open('plugins.yml', 'r')
+plugins = yaml.load(stream)
+
+# Uninstall plugins
+cmd = '$INSTALLATION_DIR/eclipse/eclipse -noSplash -clean -purgeHistory' \
+    + ' -application org.eclipse.equinox.p2.director -destination $INSTALLATION_DIR/eclipse' \
+    + ' -uninstallIU '  + ','.join(plugins['uninstall']) \
+
+subprocess.run(cmd, shell=True, check=True)
+
+# Install plugins
+for plugin_name, feature_group in plugins['install'].items():
+  print('Install ', plugin_name)
+  for features in feature_group:
+    cmd = '$INSTALLATION_DIR/eclipse/eclipse -noSplash -clean -purgeHistory' \
+        + ' -application org.eclipse.equinox.p2.director -destination $INSTALLATION_DIR/eclipse' \
+        + ' -repository ' + features['repo'] \
+        + ' -installIU '  + ','.join(features['units'])
+  
+    subprocess.run(cmd, shell=True, check=True)
