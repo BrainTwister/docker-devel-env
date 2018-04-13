bc2438057f0b1a0ecc91a0d4bd2cf10ec7ba5a61
Bernd Doser <bernd.doser@braintwister.eu>
2018-04-13 14:10:33 +0200

    add NVIDIA nsight IDE

---
+++ nsight/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Add conan repositories of conan base module for eclipse user
+if [ -f /config/conan_add_repositories.sh ]
+then
+  /config/conan_add_repositories.sh
+fi
+
+$@
---
+++ nsight/install_plugins.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import argparse
+import subprocess
+import yaml
+
+parser = argparse.ArgumentParser(description='Install eclipse plugins.')
+parser.add_argument('-p, --plugins', dest='plugins', default='plugins.yml',
+    help='List of plugins to install (default: plugins.yml)')
+
+args = parser.parse_args()
+plugins = yaml.load(open(args.plugins, 'r'));
+
+# Uninstall plugins
+if 'unistall' in plugins:
+  cmd = '/usr/local/cuda/bin/nsight -noSplash -clean -purgeHistory' \
+      + ' -application org.eclipse.equinox.p2.director -destination $INSTALLATION_DIR/eclipse' \
+      + ' -uninstallIU '  + ','.join(plugins['uninstall']) \
+
+  subprocess.run(cmd, shell=True, check=True)
+
+# Install plugins
+if 'istall' in plugins:
+  for plugin_name, feature_group in plugins['install'].items():
+    print('Install ', plugin_name)
+    for features in feature_group:
+      cmd = '/usr/local/cuda/bin/nsight -noSplash -clean -purgeHistory' \
+          + ' -application org.eclipse.equinox.p2.director -destination $INSTALLATION_DIR/eclipse' \
+          + ' -repository ' + features['repo'] \
+          + ' -installIU '  + ','.join(features['units'])
+    
+      subprocess.run(cmd, shell=True, check=True)
