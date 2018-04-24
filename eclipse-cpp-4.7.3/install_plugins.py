08f6b5df397ef66fce6d874c1bffe9e52b6a78e4
Bernd Doser <bernd.doser@braintwister.eu>
2018-04-24 11:25:39 +0200

    Increase timeout and retries for eclipse plugin installation

--- eclipse-cpp-4.7.3/install_plugins.py
+++ eclipse-cpp-4.7.3/install_plugins.py
@@ -27,6 +27,7 @@ if 'install' in plugins:
       cmd = '$INSTALLATION_DIR/eclipse/eclipse -noSplash -clean -purgeHistory' \
           + ' -application org.eclipse.equinox.p2.director -destination $INSTALLATION_DIR/eclipse' \
           + ' -repository ' + ','.join(plugins['general_repos']) + ',' + ','.join(features['repos']) \
-          + ' -installIU '  + ','.join(features['units'])
+          + ' -installIU '  + ','.join(features['units']) \
+          + ' -vmargs -Dorg.eclipse.equinox.p2.transport.ecf.retry=5 -Dorg.eclipse.ecf.provider.filetransfer.retrieve.readTimeout=10000'
     
       subprocess.run(cmd, shell=True, check=True)
