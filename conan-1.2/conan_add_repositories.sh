53d5ac8191234114d39aa63d5a361a582ddc72b4
Bernd Doser <bernd.doser@braintwister.eu>
2018-05-01 16:12:12 +0200

    update conan to 1.3

--- conan-1.2/conan_add_repositories.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-conan remote add braintwister https://api.bintray.com/conan/braintwister/conan
-conan remote add conan-community https://api.bintray.com/conan/conan-community/conan
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
