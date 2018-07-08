06d5960f10c51b49830dfb4e3f94a9c0e552fe6c
Bernd Doser <bernd.doser@braintwister.eu>
2018-07-08 22:37:17 +0200

    Fix #3: User has no permissions to update eclipse plugins

--- eclipse-cpp-4.8/entrypoint.sh
+++ eclipse-cpp-4.8/entrypoint.sh
@@ -20,6 +20,9 @@ then
   export HOME=/home/$USER_NAME
   cd $HOME
 
+  # Set user as owner of eclipse installation directory to allow updates of plugins
+  chown -R $USER_NAME:$GROUP_NAME $INSTALLATION_DIR/eclipse
+
   # Add conan repositories of conan base module for eclipse user
   if [ -f /config/conan_add_repositories.sh ]
   then
