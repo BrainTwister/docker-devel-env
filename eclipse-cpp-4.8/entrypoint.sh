ac48219fc89fa7bfd4eac06907cbf50f5d1ffdc7
Bernd Doser <bernd.doser@braintwister.eu>
2018-07-11 17:44:56 +0200

    Give permission to access /dev/tty*

--- eclipse-cpp-4.8/entrypoint.sh
+++ eclipse-cpp-4.8/entrypoint.sh
@@ -20,6 +20,9 @@ then
   export HOME=/home/$USER_NAME
   cd $HOME
 
+  # Give permission to access /dev/tty*
+  usermod -a -G dialout $USER_NAME
+
   # Set user as owner of eclipse installation directory to allow updates of plugins
   chown -R $USER_NAME:$GROUP_NAME $INSTALLATION_DIR/eclipse
 
